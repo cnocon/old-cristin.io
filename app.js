a094019bed7b708c43c5ea88580db3dc6f003173
Cristin O'Connor <588690+cnocon@users.noreply.github.com>
2017-12-27 14:18:55 +0000

    Loading HTML file with Resume at /

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 #!/usr/bin/env nodejs
-var http = require('http');
+/*var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.end('<h1>I am currently converting my website into a Node application. I apologize for the mess while I finish that up.</h1>');
 }).listen(8080, 'localhost');
+console.log('Server running at http://localhost:8080/');*/
+
+var http = require('http'),
+    fs = require('fs');
+
+fs.readFile('./index.html', function (err, html) {
+  if (err) {
+    throw err;
+  }
+  http.createServer(function(req, res) {
+    res.writeHeader(200, {"Content-Type": "text/html"});
+    res.write(html);
+    res.end();
+  }).listen(8080, 'localhost');
+});
 console.log('Server running at http://localhost:8080/');
+
